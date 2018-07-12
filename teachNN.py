55d3422220bd1b5efddafdac14a87cfe0a5731d0
Unknown <violettaff@gmail.com>
2018-07-12 20:10:32 +0300

    predict generator

--- teachNN.py
+++ teachNN.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+import numpy as np
+from intertools import product
 import math
 from keras.callbacks import LearningRateScheduler, TensorBoard
 from keras.preprocessing.image import ImageDataGenerator
@@ -7,6 +9,8 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras import regularizers
 from keras.optimizers import SGD
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
 
 train_dir = 'train'
 val_dir = 'val'
@@ -97,3 +101,37 @@ json_file = open("roadSigns.json", "w")
 json_file.write(model_json)
 json_file.close()
 model.save_weights("roadSigns_weight.h5")
+
+
+test_images, test_labels = next(train_generator)
+predictions = model.predict_generator(test_generator, steps = 1, verbose = 0)#массив предсказаний
+cm = confusion_matrix(test_labels, predictions[:, 0])
+
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+        if normalize:
+            cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+            print("Normalized confusion matrix")
+        else:
+            print('Confusion matrix, without normalization')
+
+        print(cm)
+        plt.imshow(cm, interpolation='nearest', cmap=cmap)
+        plt.title(title)
+        plt.colorbar()
+        tick_marks = np.arange(len(classes))
+        plt.xticks(tick_marks, classes, rotation=45)
+        plt.yticks(tick_marks, classes)
+
+        fmt = '.2f' if normalize else 'd'
+        thresh = cm.max() / 2.
+        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+            plt.text(j, i, format(cm[i, j], fmt),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+
+        plt.tight_layout()
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label')
+
+cm_plot_labels=['cat','dog']
+plot_confusion_matrix(cm, cm_plot_labels, title = 'confusion matrix')
