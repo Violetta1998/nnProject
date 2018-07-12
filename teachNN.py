ba93f7895aa023283e16d4327a9697480d42a56b
Unknown <violettaff@gmail.com>
2018-07-12 20:22:06 +0300

    change import modules

--- teachNN.py
+++ teachNN.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import numpy as np
-from intertools import product
+import itertools
 import math
 from keras.callbacks import LearningRateScheduler, TensorBoard
 from keras.preprocessing.image import ImageDataGenerator
@@ -25,11 +25,12 @@ nb_train_samples = 4588
 nb_validation_samples = 1375
 nb_test_samples = 1375
 
+
 def step_decay(epoch):
     initial_lrate = 0.1
     drop = 0.5
     epochs_drop = 10.0
-    lrate = initial_lrate * math.pow(drop, math.floor((1+epoch)/epochs_drop))
+    lrate = initial_lrate * math.pow(drop, math.floor((1 + epoch) / epochs_drop))
     return lrate
 
 
@@ -52,7 +53,7 @@ test_generator = datagen.flow_from_directory(
     batch_size=batch_size,
     class_mode='binary')
 
-#Слои с 1 по 6 используются для выделения важных признаков в изображении, а слои с 7 по 10 - для классификации.
+# Слои с 1 по 6 используются для выделения важных признаков в изображении, а слои с 7 по 10 - для классификации.
 model = Sequential()
 model.add(Conv2D(32, (3, 3), input_shape=input_shape))
 model.add(Activation('relu'))
@@ -73,7 +74,7 @@ model.add(Dropout(0.5))
 model.add(Dense(1))
 model.add(Activation('sigmoid'))
 
-#компилируем нейронную сеть
+# компилируем нейронную сеть
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # learning schedule callback
@@ -93,7 +94,7 @@ model.fit_generator(
     validation_steps=nb_validation_samples // batch_size)
 
 scores = model.evaluate_generator(test_generator, nb_test_samples // batch_size)
-print("Аккуратность на тестовых данных: %.2f%%" % (scores[1]*100))
+print("Аккуратность на тестовых данных: %.2f%%" % (scores[1] * 100))
 print("Test score", scores[0])
 
 model_json = model.to_json()
@@ -102,36 +103,37 @@ json_file.write(model_json)
 json_file.close()
 model.save_weights("roadSigns_weight.h5")
 
-
 test_images, test_labels = next(train_generator)
-predictions = model.predict_generator(test_generator, steps = 1, verbose = 0)#массив предсказаний
+predictions = model.predict_generator(test_generator, steps=1, verbose=0)  # массив предсказаний
 cm = confusion_matrix(test_labels, predictions[:, 0])
 
+
 def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
-        if normalize:
-            cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-            print("Normalized confusion matrix")
-        else:
-            print('Confusion matrix, without normalization')
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
 
-        print(cm)
-        plt.imshow(cm, interpolation='nearest', cmap=cmap)
-        plt.title(title)
-        plt.colorbar()
-        tick_marks = np.arange(len(classes))
-        plt.xticks(tick_marks, classes, rotation=45)
-        plt.yticks(tick_marks, classes)
+    print(cm)
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
 
-        fmt = '.2f' if normalize else 'd'
-        thresh = cm.max() / 2.
-        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-            plt.text(j, i, format(cm[i, j], fmt),
-                     horizontalalignment="center",
-                     color="white" if cm[i, j] > thresh else "black")
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
 
-        plt.tight_layout()
-        plt.ylabel('True label')
-        plt.xlabel('Predicted label')
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
 
-cm_plot_labels=['cat','dog']
-plot_confusion_matrix(cm, cm_plot_labels, title = 'confusion matrix')
+
+cm_plot_labels = ['cat', 'dog']
+plot_confusion_matrix(cm, cm_plot_labels, title='confusion matrix')
